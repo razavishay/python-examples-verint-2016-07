04d089103bf1acdbc0308b6c2af4d48309d54f28
razavishay <raz.avishay@gmail.com>
2016-07-06 09:56:24 +0300

    Solution for: sol-python-verint.syntax-lab/07.py

--- 05_syntax_lab/07.py
+++ 05_syntax_lab/07.py
@@ -1,8 +1,24 @@
-""" Write a program that selects a random number
-and asks the user to guess it.
-
-After each guess print a hint "too large" or "too small" to the user.
-
-Bonus: To make things interesting, the program should cheat once in a white
-"""
+from random import randint
+numberToGuess = randint (1,100)
+mistakeChance = 0.2 # 20% change of prompting wrong message to the user
+print "Please choose a number between 1 to 100:"
+while True:
+    userGuess = int(raw_input())
+    wrongAnswerChance = randint (0,100) # can be also outside the while loop to have it constant throgh the run
+    if userGuess >= 0 and userGuess <= 100:
+        if userGuess == numberToGuess: # right guess
+            if wrongAnswerChance > 100 * mistakeChance: # print the right answer
+                print "This is right! %d is the number" % userGuess
+                break
+            else: # give him a wrong answer
+                print "Wrong guess, try again"
+        else: # wrong guess
+             if wrongAnswerChance > 100 * mistakeChance: # print the right answer
+                 print "Wrong guess, try again"
+             else: # do nothing... we don't tell him he's right if he's wrong
+                 print "Wrong guess, try again"
+    elif userGuess > 100:
+        print "%d is greater than 100. Try again." % userGuess
+    else:
+        print "%d is lesser than 1. Try again" %userGuess
 
