b59d41d3b23f432915c35185d35c0e8cd2cebd9c
razavishay <raz.avishay@gmail.com>
2016-07-06 12:28:08 +0300

    Solution for: sol-python-verint.modules-lab/03.py

--- 06_modules_lab/03.py
+++ 06_modules_lab/03.py
@@ -1,11 +1,17 @@
-""" Write a program that searches current working directory
-for files larger than 1MB. Every time you find such a file print
-its name to the user.
+import os
+import sys
+import argparse
 
-- When the program finds a large file. It should ask the user
-  a message asking if she wants to delete it, and delete the
-  file if requested
-
-- Take threshold and path as command line arguments
-"""
-
+parser = argparse.ArgumentParser(description='Get Path and file sizes according to user input')
+parser.add_argument('-p', nargs = 1, default = '.', help = 'The Path where files are to be searched', metavar = 'PATH')
+parser.add_argument('-s', nargs = 1, default = 1, type = int, help = "Minimum file size (MB) to look for", metavar = "SIZE")
+args = parser.parse_args()
+path = args.p[0]
+sizeToSearchMega = args.s[0]
+sizeToSearchBytes = 1024 * 1024 * sizeToSearchMega
+for root, dirs, files in os.walk(path):
+    for filename in files:
+        file_path = os.path.join(root,filename)
+        statInfo = os.stat(file_path)
+        if statInfo.st_size > sizeToSearchBytes:
+            print "%s is %d MB" %(file_path, statInfo.st_size / 1024 / 1024)
