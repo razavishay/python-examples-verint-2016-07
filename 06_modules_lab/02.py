b3521252167835f906aa00aa799532dab58aff79
razavishay <raz.avishay@gmail.com>
2016-07-06 10:22:54 +0300

    Solution for: sol-python-verint.modules-lab/02.py

--- 06_modules_lab/02.py
+++ 06_modules_lab/02.py
@@ -1,4 +1,10 @@
-""" Write a program that reads 2 numbers from sys.argv
-and prints their sum.
-"""
-
+import sys
+try:
+    float(sys.argv[1])
+    float(sys.argv[2])
+except:
+    print "Input must consist of two whole numbers"
+    sys.exit()
+a = float(sys.argv[1])
+b = float(sys.argv[2])
+print "The sum of %f and %f is %f" %(a, b, a+b)
