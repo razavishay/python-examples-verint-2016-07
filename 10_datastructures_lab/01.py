8e970db9076250485be66f6ca321f2e2664f60ea
razavishay <raz.avishay@gmail.com>
2016-07-13 11:22:32 +0300

    Solution for: sol-python-verint.datastructures-lab/01.py

--- 10_datastructures_lab/01.py
+++ 10_datastructures_lab/01.py
@@ -1,10 +1,19 @@
-"""
-Write a program that takes two strings
-from the user and checks if they represent
-a valid user name.
-Valid users and passwords:
-    apple => red
-    lettuce => green
-    lemon => yellow
-    orange => orange
-"""
+import sys
+names=sys.argv[1:]
+print "names=", names
+hostsFile = "C:\\Python Course\\2\\hosts.txt"
+hostsDict = {}
+with open(hostsFile, 'r') as f:
+    for line in f:
+        ipAddr=line.split('=')[1]
+        hostname=(line.split('=')[0]).strip()
+        hostsDict.setdefault(hostname,0)
+        hostsDict[hostname]=ipAddr.strip()
+
+print hostsDict
+for n in names:
+    if n in hostsDict:
+        print "IP of %s is %s" % (n, hostsDict[n])
+    else:
+        print "%s is not on the list!" %n
+
