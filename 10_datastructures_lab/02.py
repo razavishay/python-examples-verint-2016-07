eeb798764e209ae8522b0ced1086ff7ea90938f2
razavishay <raz.avishay@gmail.com>
2016-07-13 13:55:16 +0300

    Solution for: sol-python-verint.datastructures-lab/02.py

--- 10_datastructures_lab/02.py
+++ 10_datastructures_lab/02.py
@@ -1,5 +1,26 @@
-"""
-Write a program that prints only unique values
-from sys.argv (without program name)
-"""
+import sys
+from collections import Counter
+print "Please give me a file"
+wordsFile=raw_input()
+words=[]
+counterList=[]
+dict={}
+with open(wordsFile, 'r') as f:
+    for line in f:
+        word=line.strip()
+        words.append(word)
+        cnt=Counter(word)
+        if cnt not in counterList:
+            counterList.append(cnt) 
 
+for cnt in counterList:
+    for word in words:
+        if Counter(word)==cnt:
+            print word, " ",
+    print
+
+
+            
+        
+        
+        
