efebf3054d233809bb436c5f4eb7e9c966bd82ef
razavishay <raz.avishay@gmail.com>
2016-07-14 12:28:24 +0300

    Solution for: sol-python-verint.functions-lab/03.py

--- 14_functions_lab/03.py
+++ 14_functions_lab/03.py
@@ -1,5 +1,14 @@
-"""
-Write a function that calculates the sum
-of the 10th digit from all arguments passed to it
-"""
+def TensSum(*list):
+    s = 0
+    for number in list:
+        if type(number) == int:
+            numberAsString = str(number)
+            if len(numberAsString) < 2:
+                s += 0
+            else:
+                s += int(numberAsString[-2:-1])
+    return s
 
+
+
+print TensSum (10,20,360,1,60)
