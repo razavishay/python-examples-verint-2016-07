cb4169b444bb6926307646dbbd8b25d7d4c67828
razavishay <raz.avishay@gmail.com>
2016-07-17 15:57:11 +0300

    Solution for: sol-python-verint.regexp-lab/03.py

--- 18_regexp_lab/03.py
+++ 18_regexp_lab/03.py
@@ -1,15 +1,9 @@
-"""
-Write a python program that takes a CSV file
-reads it line by line and prints each line
-with first and second columns reversed.
+import re
+import sys
 
-Sample input:
-    Shana,Sargent,shanasargent@isoswitch.com
-    Witt,Hampton,witthampton@zaphire.com
-    Morgan,Grant,morgangrant@lotron.com
+filename = sys.argv[1]
 
-Sample output:
-    Sargent,Shana,shanasargent@isoswitch.com
-    Hampton,Witt,witthampton@zaphire.com
-    Grant,Morgan,morgangrant@lotron.com
-"""
+with open (filename, 'r') as f:
+    for line in f:
+        sys.stdout.write(re.sub(r'(\w+),(\w+),(.*)', lambda m: m.group(2)+ ',' + m.group(1)+ ',' + m.group(3), line))
+        
