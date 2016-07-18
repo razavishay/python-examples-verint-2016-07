7ca81021dcf7904309180d06f0346e2fb5abbc34
razavishay <raz.avishay@gmail.com>
2016-07-18 10:48:23 +0300

    Solution for: sol-python-verint.class-syntax-lab/03.py

--- 20_classes_lab/03.py
+++ 20_classes_lab/03.py
@@ -1,13 +1,33 @@
-"""
-Following code assumes a class named Widget that depends on
-other widgets
-Widget ctor takes a name, and function 
-add_dependency(depends)
-marks widget as depends on all widgets in "depends" list
+class Widget (object):
+    def __init__(self, val = ""):
+        self.name = val
+        self.depends = []
 
-When building a widget, you need to first build all the widgets it
-depends on.
+    def add_dependency(self, *dependencies):
+        for dependency in dependencies:
+            self.depends.append(dependency)
 
-Implement widget so the following code works
-"""
+    def build(self):
+        if self.depends != []:
+            for widget in self.depends:
+                print widget.name
+                
 
+luke = Widget("Luke")
+hansolo = Widget("Han Solo")
+leia = Widget("Leia")
+yoda = Widget("Yoda")
+padme = Widget("Padme Amidala")
+anakin = Widget("Anakin Skywalker")
+obi = Widget("Obi-Wan")
+darth = Widget("Darth Vader")
+_all = Widget("All")
+
+
+luke.add_dependency(hansolo, leia, yoda)
+leia.add_dependency(padme, anakin)
+obi.add_dependency(yoda)
+darth.add_dependency(anakin)
+
+_all.add_dependency(luke, hansolo, leia, yoda, padme, anakin, obi, darth)
+_all.build()
