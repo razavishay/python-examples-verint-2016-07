3e9eeb4b2b5904572c3a8f8efd243915cbbb091a
razavishay <raz.avishay@gmail.com>
2016-07-18 10:48:25 +0300

    Solution for: sol-python-verint.class-syntax-lab/02.py

--- 20_classes_lab/02.py
+++ 20_classes_lab/02.py
@@ -1,4 +1,10 @@
-"""
-Following code assumes a class named MyCounter
-Fill in the code so correct result is printed
-"""
+class MyCounter(object):
+    count = 0
+    def __init__(self, count = 0):
+        MyCounter.count += 1        
+
+for _ in range(10):
+    c1 = MyCounter()
+
+# should print 10
+print MyCounter.count
